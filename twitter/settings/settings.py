8087000a1950ac6c32b507c7aadcba6485b6f9a0
hhio67 <hhio.67.2020@gmail.com>
2021-01-21 23:39:51 +0900

    パスの環境変数（よくわからん）

--- twitter/settings/settings.py
+++ twitter/settings/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-dotenv_path = join("/Users/hhio6/Desktop/line-semi-auto/semiauto", ".env")
+dotenv_path = join(os.environ.get("PATH"), ".env")
 load_dotenv(dotenv_path)
 
 TPS = os.environ.get("TWITTER_PASS")
